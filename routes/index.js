68a79ec54ca928e21baa264a1a0cbacc5a87ef5c
Smurf-Hunter <sarwan010300@gmail.com>
2021-05-20 22:02:42 +0530

    Final Release

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 const fetch = require("node-fetch");
+const schedule = require('node-schedule');
+const Functions = require('../public/javascripts/Functions');
+const request = require('request-promise');
+
+const job = schedule.scheduleJob('*/30 * * * *',async function(){
+    request.get(Functions.getUrl()+'sendAlerts').then(function(body){
+      console.log("Checked for vaccinations");
+    })
+  });
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
