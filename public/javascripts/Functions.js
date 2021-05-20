386f1b7cff59a4e6ca77ffe7daf1067c51b65df2
Smurf-Hunter <sarwan010300@gmail.com>
2021-05-20 14:31:57 +0530

    Hosted API

--- public/javascripts/Functions.js
+++ public/javascripts/Functions.js
@@ -14,7 +14,7 @@ const updateAndCreate = {
 
 var localHostUrl = "http://localhost:8000/";
 
-var hostedUrl = "";
+var hostedUrl = "http://covidvaccinationalerts-env.eba-hgbvgfjc.ap-south-1.elasticbeanstalk.com/";
 
 const request = require('request-promise').defaults({jar:true});
 
@@ -51,7 +51,7 @@ async function getCurrentIndianDate(){
 module.exports = {
 
     getUrl : function(){
-        return localHostUrl;
+        return hostedUrl;
     },
 
     getMongoUrl : function(){
