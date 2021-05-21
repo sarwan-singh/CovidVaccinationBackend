282db2aab875a1d4889e4ee01c0de54758ed5a0a
Smurf-Hunter <sarwan010300@gmail.com>
2021-05-21 10:51:37 +0530

    Final Release

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,13 @@ var router = express.Router();
 const fetch = require("node-fetch");
 const Functions = require('../public/javascripts/Functions');
 const request = require('request-promise');
+const schedule = require('node-schedule')
+
+var event = schedule.scheduleJob("*/30 * * * *", function() {
+    request.get('https://covidvaccinationalerts-env-1.eba-hgbvgfjc.ap-south-1.elasticbeanstalk.com/sendAlerts').then(function(body){
+    console.log("Vaccination requests sent!!!");
+  })
+});
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
