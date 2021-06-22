7eb068b3e96c23334b0c9ce2955fd411d3ea460b
Smurf-Hunter <sarwan010300@gmail.com>
2021-06-22 18:36:00 +0530

    Added a new server for failed mails

--- public/javascripts/Services/MailService.js
+++ public/javascripts/Services/MailService.js
@@ -3,9 +3,9 @@ var Functions = require('../Functions');
 var SecurityService = require('./SecurityService');
 var GenerateMailService = require('./GenerateMailService')
 
-function generateMailOptions(email, age, district, districtName, dose, type, centers){
+function generateMailOptions(from, email, age, district, districtName, dose, type, centers){
     var mailOptions = {
-        from: 'helpsws2.0@gmail.com',
+        from: from,
         to: email,
         subject: '',
         html: ""
@@ -37,35 +37,69 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+var transporter2 = nodemailer.createTransport({
+    service: 'gmail',
+    secure: true,
+    pool: true,
+    auth: {
+      user: 'help.covidvaccination2@gmail.com',
+      pass: 'Qwerty@100'
+    }
+})
+
+var from1 = "help.covidvaccination@gmail.com";
+var from2 = "help.covidvaccination2@gmail.com"
 module.exports = {
 
     sendRequestStart : async function(email, age, district, districtName, dose, content = []){
-      transporter.sendMail(generateMailOptions(email, age, district, districtName, dose, 1, content), function(error, info){
+      
+      transporter1.sendMail(generateMailOptions(from1, email, age, district, districtName, dose, 1, content), function(error, info){
         if (error) {
-          console.error(error);
+          console.log("ERROR : FIRST SERVER FULL");
+          transporter2.sendMail(generateMailOptions(from2, email, age, district, districtName, dose, 1, content), function(error, info){
+            if (error) {
+              console.log("ERROR : SECOND SERVER FULL");
+            } else {
+              // console.log('Email sent from server 2 : ' + info.response);
+            }
+          });
         } else {
-          console.log('Email sent: ' + info.response);
+          // console.log('Email sent: ' + info);
         }
       });
     },
 
     sendRequestEnd : async function(email, age, district, districtName, dose, content = []){
-      transporter.sendMail(generateMailOptions(email, age, district, districtName, dose, 3, content), function(error, info){
+      transporter.sendMail(generateMailOptions(from1, email, age, district, districtName, dose, 3, content), function(error, info){
         if (error) {
-          console.log(error);
+          console.log("ERROR : FIRST SERVER FULL");
+          transporter2.sendMail(generateMailOptions(from2, email, age, district, districtName, dose, 3, content), function(error, info){
+            if (error) {
+              console.log("ERROR : SECOND SERVER FULL");
+            } else {
+              // console.log('Email sent: ' + info.response);
+            }
+          });
         } else {
-          console.log('Email sent: ' + info.response);
+          // console.log('Email sent: ' + info.response);
         }
       });
     },
 
     sendAlert : async function(email, age, district, districtName, dose, centers){
       
-      transporter.sendMail(generateMailOptions(email, age, district, districtName, dose, 2, centers),  function(error, info){
+      transporter.sendMail(generateMailOptions(from1, email, age, district, districtName, dose, 2, centers),  function(error, info){
         if (error) {
-          console.log(error);
+          console.log("ERROR : FIRST SERVER FULL");
+          transporter2.sendMail(generateMailOptions(from2, email, age, district, districtName, dose, 2, centers),  function(error, info){
+            if (error) {
+              console.log("ERROR : SECOND SERVER FULL");
+            } else {
+              // console.log('Email sent: ' + info.response + '\n email : ' + email + ' district : ' + districtName);
+            }
+          })
         } else {
-          console.log('Email sent: ' + info.response + '\n email : ' + email + ' district : ' + districtName);
+          // console.log('Email sent: ' + info.response + '\n email : ' + email + ' district : ' + districtName);
         }
       })
     }
